f3514b83ae32868cfaa6e65bd7e758627761a301
Abubakar Mughal <64458681+abubakar2000@users.noreply.github.com>
2022-02-12 16:21:58 +0500

    landing page done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './View/about-us/about-us.component';
+import { ContactComponent } from './View/contact/contact.component';
+import { LandingPageComponent } from './View/landing-page/landing-page.component';
+import { NewsComponent } from './View/news/news.component';
+import { ServicesComponent } from './View/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent, pathMatch:"full" },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'services', component: ServicesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { LandingPageComponent } from './View/landing-page/landing-page.component
 import { NavigationComponent } from './Components/navigation/navigation.component';
 import { SiteIdentityComponent } from './Components/site-identity/site-identity.component';
 import { EstimateBoxComponent } from './Components/estimate-box/estimate-box.component';
+import { ServicesComponent } from './View/services/services.component';
+import { AboutUsComponent } from './View/about-us/about-us.component';
+import { NewsComponent } from './View/news/news.component';
+import { ContactComponent } from './View/contact/contact.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +21,11 @@ import { EstimateBoxComponent } from './Components/estimate-box/estimate-box.com
     NavigationComponent,
     SiteIdentityComponent,
     EstimateBoxComponent,
+    ServicesComponent,
+    AboutUsComponent,
+    NewsComponent,
+    ContactComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
